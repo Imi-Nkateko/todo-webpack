00a2b9f528aea276830b9efcae8079f69f89a3eb
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-10-07 20:11:28 +0200

    create a todo class

--- src/todo.js
+++ src/todo.js
@@ -1,1 +1,13 @@
-export const todo = "clean your bedroom";
+//export const todo = "clean your bedroom";
+
+class todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+const newTodo = new todo("blah", "lorem100", "2025-06-26", "important");
+console.log(newTodo);
