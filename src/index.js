21a5a66e42671173cfd22e745815309aa9c17d49
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-10-09 23:21:47 +0200

    Create a fresh object from current form input

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 // index.js
 import "./styles.css";
 import "./modal.css";
-//import { todo } from "./todo.js";
-
-//console.log(todo);
+import { title, description, dueDate, priority } from "./modal";
 
 // Get the modal, open button, and close button elements
-import { modal, openBtn, closeBtn } from "./modal";
+import { modal, openBtn, closeBtn, addTodoBtn, mainContainer } from "./modal";
 
 // When the user clicks the open button, display the modal
 openBtn.onclick = function() {
@@ -25,3 +23,32 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 };
+
+
+import { createNewTodo } from "./todo";
+
+addTodoBtn.onclick = function () {
+  const newTodo = createNewTodo(); // Create a fresh object from current form input
+
+  const todoElement = document.createElement("div");
+  todoElement.classList.add("todo");
+
+  todoElement.innerHTML = `
+    <div>
+      <h1>${newTodo.title}</h1>
+      <p>${newTodo.description}</p>
+      <span>${newTodo.dueDate}</span>
+      <h2>${newTodo.priority}</h2>
+    </div>`;
+
+  mainContainer.appendChild(todoElement);
+
+  // Optional: reset form and close modal
+  title.value = "";
+  description.value = "";
+  dueDate.value = "";
+  priority.value = "";
+
+  modal.style.display = "none";
+};
+
