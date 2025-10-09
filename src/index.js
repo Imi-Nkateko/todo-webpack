85eb62b5e0a7589eb46427f37cebe157bfbfebf4
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-10-09 20:17:50 +0200

    added modal popup and move code to correct files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 // index.js
 import "./styles.css";
-import "./modal.css"
-import { todo } from "./todo.js";
+import "./modal.css";
+//import { todo } from "./todo.js";
 
-console.log(todo);
+//console.log(todo);
+
+// Get the modal, open button, and close button elements
+import { modal, openBtn, closeBtn } from "./modal";
+
+// When the user clicks the open button, display the modal
+openBtn.onclick = function() {
+  modal.style.display = "block";
+  console.log("clicked");
+};
+
+// When the user clicks on the close button, hide the modal
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal content, hide the modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
--- src/modal.js
+++ src/modal.js
@@ -1,21 +1,4 @@
 // Get the modal, open button, and close button elements
-const modal = document.getElementById("myModal");
-const openBtn = document.getElementById("openModalBtn");
-const closeBtn = document.querySelector(".close-btn");
-
-// When the user clicks the open button, display the modal
-openBtn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on the close button, hide the modal
-closeBtn.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal content, hide the modal
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+export const modal = document.getElementById("myModal");
+export const openBtn = document.getElementById("openModalBtn");
+export const closeBtn = document.querySelector(".close-btn");
