368de39b61b3fcd6035ea4b47c621de3742768c7
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-10-09 23:22:58 +0200

    export elements for readability

--- src/modal.js
+++ src/modal.js
@@ -2,3 +2,13 @@
 export const modal = document.getElementById("myModal");
 export const openBtn = document.getElementById("openModalBtn");
 export const closeBtn = document.querySelector(".close-btn");
+
+// Get the form input, button elements
+export const title = document.getElementById("title-el");
+export const description = document.getElementById("description-el");
+export const priority = document.getElementById("priority-el");
+export const dueDate = document.getElementById("dueDate-el");
+export const addTodoBtn = document.getElementById("add-todo");
+
+// Get the main element
+export const mainContainer = document.querySelector("main");
